6b4b31a7dbc4185bf260c0c8e34b950515e41315
wiammmmmmm <wiambouda126@gmail.com>
2025-09-06 21:42:17 +0200

    update

--- event/src/app/menu/header-menu.component.ts
+++ event/src/app/menu/header-menu.component.ts
@@ -34,8 +34,53 @@ export class HeaderMenuComponent {
         { name: "Transport", submenu: [{ name: "Chaise ergonomique" }, { name: "Fauteuil de direction" }] }
       ]
     },
-    // ... (le reste de vos données)
-  ];
+  {
+        name: "Immobolier et Lieux",
+        icon: '<i class="fa-solid fa-building menu-icon"></i>',
+        submenu: [
+            { name: "Lieux pour evenements", submenu: [{ name: "Reception" }, { name: "Salle de spectacle" }, { name: "Seminaire" }, { name: "Conference" }, { name: "Reunion" }, { name: "Exposition" }, { name: "Auditorium" }, { name: "Discotheque" }] },
+            { name: "Lieux de Sports", submenu: [{ name: "Squash" }, { name: "Tennis" }, { name: "Foot" }, { name: "Basket" }, { name: "Salle OmniSport" }, { name: "Salle de sport" }, { name: "Piscine" }] },
+            { name: "Immo", submenu: [{ name: "Bureaux" }, { name: "Studios" }, { name: "Chambre" }, { name: "Appartement" }, { name: "Maison" }] }
+        ]
+    },
+    {
+        name: "Materiel - Vehicule - Engins",
+        icon: '<i class="fa-solid fa-screwdriver-wrench menu-icon"></i>',
+        submenu: [
+            { name: "Vehicules et Engins (Route , Mer, Air)", submenu: [{ name: "Vehicule Route" }, { name: "Materiels de mers et air" }] },
+            { name: "Travaux, Chantier et Espace Vert", submenu: [{ name: "Outillage" }, { name: "Materiel" }, { name: "Batiment" }, { name: "Espace vert" }] },
+            { name: "Materiels de Fete et Restauration", submenu: [{ name: "Fete et reception" }, { name: "Restauration" }] },
+            { name: "Sport et Loisirs", submenu: [{ name: "Sport d'interieur" }, { name: "Sport de glisse" }, { name: "Clyclisme" }, { name: "Loisir Moteur" }, { name: "Fitness" }, { name: "Aerobic" }, { name: "Musculation" }, { name: "Canyonning" }] }
+        ]
+    },
+    {
+        name: "Multimedia",
+        icon: '<i class="fa-solid fa-laptop menu-icon"></i>',
+        submenu: [
+            { name: "Informatique - Electronique - Mobile", submenu: [{ name: "Electronique" }] },
+            { name: "Son & Audio & Lumiere", submenu: [{ name: "Retroprojecteur" }, { name: "Ecran de projection" }, { name: "Videoprojecteur" }, { name: "Projecteur de diapositives" }] },
+            { name: "Musique & Photo", submenu: [{ name: "Batteries et Percussion" }, { name: "Instruments a vent" }] }
+        ]
+    },
+    {
+        name: "Famille et Foyer",
+        icon: '<i class="fa-solid fa-house menu-icon"></i>',
+        submenu: [
+            { name: "Maison & Confort", submenu: [{ name: "Literie" }, { name: "Rangement" }] },
+            { name: "Puerecultrice", submenu: [{ name: "Poussette" }, { name: "Siege-auto" }] },
+            { name: "Sante et Paramedical", submenu: [{ name: "Bureau droit" }, { name: "Bureau d'angle" }] },
+            { name: "Habillement et Accessoires", submenu: [{ name: "Vetement" }] }
+        ]
+    },
+    {
+        name: "Service Professionnel",
+        icon: '<i class="fa-solid fa-user-tie menu-icon"></i>',
+        submenu: [
+            { name: "Personnel" },
+            { name: "Artisans", submenu: [{ name: "Reception" }, { name: "Travaux" }] },
+            { name: "Sujet Secondaire", submenu: [{ name: "Bureau droit" }, { name: "Bureau d'angle" }] }
+        ]
+    }  ];
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
